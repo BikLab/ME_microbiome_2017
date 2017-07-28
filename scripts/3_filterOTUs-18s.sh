31597ca3aaec496459ce7538a86530c19f480c57
Taruna <ta2007@wildcats.unh.edu>
2017-07-28 13:44:01 -0700

    added 18S fitering cmds

--- scripts/3_filterOTUs-18s.sh
+++ scripts/3_filterOTUs-18s.sh
@@ -20,17 +20,23 @@ REF=/rhome/taruna/shared/taruna/GOMRI/ref-dbs/SILVA_128_QIIME_release/rep_set/re
 TAXA=/rhome/taruna/shared/taruna/GOMRI/ref-dbs/SILVA_128_QIIME_release/taxonomy/18S_only/99/consensus_taxonomy_7_levels.txt
 REFALIGN=/rhome/taruna/shared/taruna/GOMRI/ref-dbs/SILVA_128_QIIME_release/rep_set_aligned/99/99_otus_aligned.fasta
 OTUS=/rhome/taruna/shared/taruna/memb/18S/analysis-results/uclust_openref99_MEMB1_18s_mergedONLY_26July2017
-
+BLANKSAMPLES=/rhome/taruna/shared/taruna/memb/16S/qiime-files/mapping/MEMB-blank-SampleIDs.txt
 
 # removes gaps from the Pynast aligned seqs 
 filter_alignment.py -i $OTUS/pynast_aligned_seqs/rep_set_aligned.fasta \
 	-o $OTUS/pynast_aligned_seqs \
 	--suppress_lane_mask_filter
 	
-# filter BIOM table for _Nematoda only
+# filter BIOM table for D_3__Metazoa (Animalia) only
 filter_taxa_from_otu_table.py \
-	-i $OTU/otu_table_mc2_w_tax.biom \
-	-o $OTU/filtered-biom-tables/otu_table_bac_firm_only.biom \
-	-p D_9__Nematoda
+	-i $OTUS/otu_table_mc2_w_tax.biom \
+	-o $OTUS/filtered-biom-tables/otu_table_mc2_w_tax_metazoaONLY.biom \
+	-p "D_3__Metazoa (Animalia)"
 
+
+# filter the otu_table_mc2_w_tax_metazoaONLY.biom to remove samples that have no OTU counts
+filter_samples_from_otu_table.py -i $OTUS/filtered-biom-tables/otu_table_mc2_w_tax_metazoaONLY.biom -o $OTUS/filtered-biom-tables/otu_table_mc2_w_tax_metazoaONLY_allNemaSamples.biom -n 1
+
+
+filter_otus_from_otu_table.py -i $OTUS/otu_table_mc2_w_tax.biom -o $OTUS/filtered-biom-tables/otu_table_mc2_w_tax_NOmetazoa.biom -e $OTUS/filtered-biom-tables/list-of-otus-MetazoaONLY.txt
 
