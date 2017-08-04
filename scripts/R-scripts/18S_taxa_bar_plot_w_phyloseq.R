446279d821063fa62e70e2dc4b5e5d30d8e934c4
Taruna <ta2007@wildcats.unh.edu>
2017-08-04 05:51:25 -0700

    updating

--- scripts/R-scripts/18S_taxa_bar_plot_w_phyloseq.R
+++ scripts/R-scripts/18S_taxa_bar_plot_w_phyloseq.R
@@ -8,8 +8,8 @@ library(wesanderson)
 map <- import_qiime_sample_data("/Users/Taruna/Dropbox/bik_lab/MEmicrobiome/files-4-phyloseq/18S/18S-euk-QIIME-mapping-MEmicrobiome-FINAL-26Jul17.txt")
 
 #import biom tables into phyloseq
-otu_metazoaRemoved <- import_biom("/Users/Taruna/Dropbox/bik_lab/MEmicrobiome/files-4-phyloseq/18S/otu_table_mc2_w_tax_BlankOTUsRemoved_BlankSamplesRemoved_NObadnems_noZeros_MetazoaRemoved-above-500.biom")
-otu_nemsRemoved <- import_biom("/Users/Taruna/Dropbox/bik_lab/MEmicrobiome/files-4-phyloseq/18S/otu_table_mc2_w_tax_BlankOTUsRemoved_BlankSamplesRemoved_NObadnems_noZeros_NemsRemoved_above-500.biom")
+otu_metazoaRemoved <- import_biom("/Users/Taruna/Dropbox/bik_lab/MEmicrobiome/files-4-phyloseq/18S/otu_table_mc2_w_tax_BlankOTUsRemoved_BlankSamplesRemoved_NObadnems_noZeros_MetazoaRemoved-above-500-kept.biom")
+otu_nemsRemoved <- import_biom("/Users/Taruna/Dropbox/bik_lab/MEmicrobiome/files-4-phyloseq/18S/otu_table_mc2_w_tax_BlankOTUsRemoved_BlankSamplesRemoved_NObadnems_noZeros_NemsRemoved_above-500-kept.biom")
 
 #merge mapping file and otu tables into a phyloseq object 
 physeq_metazoaRemoved = merge_phyloseq(otu_metazoaRemoved,map)
@@ -31,11 +31,11 @@ rank_names(physeq_metazoaRemoved_trans)
 #plot based on raw Rank2 (Phylum) data
 
 pdf('~/Desktop/18S-metazoaRemoved_Rank2.pdf')
-plot_bar(physeq_metazoaRemoved_trans, fill="Rank2")
+plot_bar(physeq_metazoaRemoved_trans, fill="Rank3")
 dev.off()
 
 pdf('~/Desktop/18S-nemsRemoved_Rank2.pdf')
-plot_bar(physeq_nemsRemoved_trans, fill="Rank2")
+plot_bar(physeq_nemsRemoved_trans, fill="Rank3")
 dev.off()
 
 
@@ -43,10 +43,15 @@ dev.off()
 ######################### Remove unobserved OTUs - Rank2 ######################### 
 any(taxa_sums(physeq_metazoaRemoved_trans) == 0)
 sum(taxa_sums(physeq_metazoaRemoved_trans) == 0)
-
 physeq_metazoaRemoved_trans_trimmed = prune_taxa(taxa_sums(physeq_metazoaRemoved_trans) > 0, physeq_metazoaRemoved_trans)
-plot_bar(physeq_metazoaRemoved_trans_trimmed, x = "Sample", y ="Abundance", fill = "Rank2")
+plot_bar(physeq_metazoaRemoved_trans_trimmed, x = "Sample", y ="Abundance", fill = "Rank5")
 
+
+
+any(taxa_sums(physeq_nemsRemoved_trans) == 0)
+sum(taxa_sums(physeq_nemsRemoved_trans) == 0)
+physeq_nemsRemoved_trans_trimmed = prune_taxa(taxa_sums(physeq_nemsRemoved_trans) > 0, physeq_nemsRemoved_trans)
+plot_bar(physeq_nemsRemoved_trans_trimmed, x = "Sample", y ="Abundance", fill = "Rank2")
 
 ############################# Remove unobserved OTUs - top taxa - Rank2 #############################
 
@@ -54,5 +59,5 @@ plot_bar(physeq_metazoaRemoved_trans_trimmed, x = "Sample", y ="Abundance", fill
 TopTenOTUs = names(sort(taxa_sums(physeq_metazoaRemoved_trans_trimmed), TRUE)[1:20])
 head(TopTenOTUs)
 physeqtop10_metazoaRemoved = prune_species(TopTenOTUs, physeq_metazoaRemoved_trans_trimmed)
-plot_bar(physeqtop10_metazoaRemoved, x = "Sample", y ="Abundance", fill = "Rank3")
+plot_bar(physeqtop10_metazoaRemoved, x = "Sample", y ="Abundance", fill = "Rank6")
 
